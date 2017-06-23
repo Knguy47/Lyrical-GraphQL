d6f5dc5baf3eae75fcaac8358c39666ea688b1e7
Kevin Nguyen <knguy47@gmail.com>
2017-06-23 10:41:04 -0500

    Setup React Router and Add create, delete song features

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://kevin:kevin1@ds057954.mlab.com:57954/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
